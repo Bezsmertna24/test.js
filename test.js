60a11709daeb8c8b0f021d76ae179ee723a9836a
Yulia Bezsmertna <yuliabezsmertna80@gmail.com>
2025-04-23 18:51:23 +0300

    mm

--- test.js
+++ test.js
@@ -610,4 +610,53 @@
 // console.log(bestScore);
 // console.log(worstScore);
 
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15, propD: 20 };
 
+// const third = { ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15, propD: 20 }
+
+// const fourth = { ...second, ...first };
+// console.log(fourth); // { propA: 5, propB: 10, propC: 50, propD: 20 }
+
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15 };
+
+// const third = { propB: 20, ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15 }
+
+// const fourth = { ...first, ...second, propB: 20 };
+// console.log(fourth); // { propA: 5, propB: 20, propC: 15 }
+
+// const fifth = { ...first, propB: 20, ...second };
+// console.log(fifth); // { propA: 5, propB: 20, propC: 15 }
+
+// const objA = { 
+// 	x: 1, 
+// 	y: 2, 
+// };
+
+// const objB = { 
+// 	y: 3, 
+// };
+
+// const result = {
+// 	x: 5, ...objA,
+// 	y: 10, ... objB,
+// 	z: 15
+// }
+
+// const defaultSettings = {
+//   theme: "light",
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+
+// const finalSettings = { ...defaultSettings,...overrideSettings};
